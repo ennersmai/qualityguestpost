32df6fad8cc855ce04fada0ff3cf23edd2428505
ennersmai <ennersmai@github.com>
2025-09-24 13:21:28 +0300

    layout fix and placeholders 4

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
           </Button>
         </form>
         <p className="text-center">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link href="/register" className="text-blue-600 hover:underline">
             Sign up
           </Link>
